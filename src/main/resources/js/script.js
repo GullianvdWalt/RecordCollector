dc34b21ed8cef1273259650beb40e6aac0c76a00
GullianvdWalt <51962834+GullianvdWalt@users.noreply.github.com>
2020-06-06 11:26:44 +0200

    Fixed display of home page. Moved images into folder.

--- src/main/resources/js/script.js
+++ src/main/resources/js/script.js
@@ -4,3 +4,24 @@ input.addEventListener("focus", function(){
 
 });
 
+
+
+function filterFunction(){
+    var input, filter, table, tr, td, i, txtValue; 
+    input = document.getElementById("artistFilter");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("myTable");
+    tr = table.getElementsByTagName("tr");
+
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[0];
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }       
+    }
+}
