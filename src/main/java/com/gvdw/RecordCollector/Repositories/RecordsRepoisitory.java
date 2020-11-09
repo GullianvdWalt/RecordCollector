4b67160509b522036c27448f8f2400e5057c571b
GullianvdWalt <51962834+GullianvdWalt@users.noreply.github.com>
2020-11-09 14:59:00 +0200

    Added_Images

--- src/main/java/com/gvdw/RecordCollector/Repositories/RecordsRepoisitory.java
+++ src/main/java/com/gvdw/RecordCollector/Repositories/RecordsRepoisitory.java
@@ -14,14 +14,10 @@
 package com.gvdw.RecordCollector.Repositories;
 
 //Imports
-import org.springframework.data.jpa.repository.JpaRepository;
 import com.gvdw.RecordCollector.Models.Records;
-import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
@@ -34,6 +30,3 @@ public interface RecordsRepoisitory extends PagingAndSortingRepository<Records, 
                 + " OR records.format LIKE ?1%", nativeQuery = true)
         public Page<Records> findAll(String keyword, Pageable pageable);
 }
-// OR records.catalog_number LIKE '%?1%' " 
-//                + " '%?1%' OR records.cover_condition LIKE '%?1%' OR records.record_condition LIKE '%?1%' OR " 
-//                + "records.pressing LIKE '%?1%' OR records.remarks LIKE '%?1%' OR records.year LIKE '%?1%'
